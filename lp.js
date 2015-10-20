a033f69f26a0da6db6bbdf830ce3d1dbb5ce3d2e
Dave Artus <djna01@gmail.com>
2015-10-20 22:29:28 +0100

    add nouns
    Extend to drive from other data sets, using object attributes

--- lp.js
+++ lp.js
@@ -1,34 +1,39 @@
 
 var fs = require('fs');
 var inquirer = require("inquirer");
- 
 
-exercise(
-    {
-        toCheck: [
-	     "Infinitive",
-	     "Gerund",
-	     "Yo",
-	     "Tú",
-	    "Él, Ella, Usted",
-	    "Nosotros",
-	    "Vosotros",
-	    "Usteds",
-	    "Vos"
-	  ] ,
-        checkValues: {
-	    "Infinitive" : "dormir",
-	    "Gerund": "duermiendo",
-	    "Yo": "duermo",
-	    "Tú": "duermes",
-	    "Él, Ella, Usted": "duerme",
-	    "Nosotros": "dormimos",
-	    "Vosotros": "dormis",
-	    "Usteds": "duermen",
-	    "Vos": "dormis"
-        } 
-    }
-)
+var collection  = process.argv[2];
+var item  = process.argv[3];
+
+if ( collection && item ) {
+
+    fs.readFile( collection + '.data', 'utf8', function (err, data) {
+        if (err) {
+            console.log("Error: " + err);
+            process.exit(1);
+        }
+
+        var parsedData  = JSON.parse(data);
+        // console.log(parsedData);
+        if ( parsedData && parsedData[item] ) {
+            var practiceDefinition = {};
+            practiceDefinition.toCheck = parsedData.toCheck;
+            practiceDefinition.checkValues = parsedData[item];
+            practiceDefinition.description = parsedData.description;
+            practiceDefinition.item = item;
+            exercise(practiceDefinition);
+        } else {
+            console.log("No practice definition for " + item);
+            process.exit(1);
+        }
+
+    });
+
+} else {
+    console.log("Usage: node lp.js <collection> <item>");
+    process.exit(1);
+}
+
 
 function exercise( practiceDefinition )
 {
@@ -38,26 +43,36 @@ function exercise( practiceDefinition )
 
     var questions = [];
 
-    for ( var i = 0; i < toCheck.length; i++) {
-	    questions[i] =     {
-		  type: 'input',
-		  name: toCheck[i],
-		  message: toCheck[i]
-		};
-	    }
+    // for ( var i = 0; i < toCheck.length; i++) {
+    //     questions[i] =     {
+    //         type: 'input',
+    //         name: toCheck[i],
+    //         message: toCheck[i]
+    //     };
+    // }
 
-    console.log("Sleep");
+    Object.keys(practiceDefinition.checkValues).forEach(function(key) {
+        questions.push(
+            {
+                type: 'input',
+                name: key,
+                message: key,
+            }
+        )
+    });
+
+    console.log(practiceDefinition.description + " " + practiceDefinition.item);
 
 
     inquirer.prompt(questions, function(response) {
-	console.log(response);
-       
-	for ( var i = 0; i < toCheck.length; i++) {
-	    if ( response[toCheck[i]] != checkValues[toCheck[i]] ){
-		 console.log(response[toCheck[i]] + "=>" 
-		       + checkValues[toCheck[i]] );
-	    }
-	}
+        console.log(response);
+
+        Object.keys(practiceDefinition.checkValues).forEach(function(key) {
+            if ( response[key] != checkValues[key] ){
+                console.log(response[key] + "=>"
+                + checkValues[key] );
+            }
+        })
 
     });
 }
